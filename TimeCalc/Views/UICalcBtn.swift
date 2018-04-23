3a7ec621a7769f8c2bec758f7d7e8d3bc6cf03d5
Donovan Campbell-Gillies <dcgillies@Franks-Mac-mini.frontierlocal.net>
2018-04-23 13:25:14 -0700

    build basic interface

---
+++ TimeCalc/Views/UICalcBtn.swift
@@ -0,0 +1,21 @@
+//
+//  UICalcBtn.swift
+//  TimeCalc
+//
+//  Created by Donovan Campbell-Gillies on 4/23/18.
+//  Copyright © 2018 CG Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class UICalcBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
